9b9ee378fcd6638eb0fee72133bef5a1da9869fc
Trung Anh <trunganhbtc@gmail.com>
2019-09-18 08:59:45 +0700

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,8 +38,8 @@ class UserTest < ActiveSupport::TestCase
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+      end
     end
-  end
 
     test "email addresses should be unique" do
     duplicate_user = @user.dup
@@ -72,7 +72,37 @@ class UserTest < ActiveSupport::TestCase
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
+      end
+    end
+
+    test "should follow and unfollow a user" do
+      michael = users(:michael)
+      archer  = users(:archer)
+      assert_not michael.following?(archer)
+      michael.follow(archer)
+      assert michael.following?(archer)
+      assert archer.followers.include?(michael)
+      michael.unfollow(archer)
+      assert_not michael.following?(archer)
+    end
+    test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
     end
   end
+
+
 
 end
